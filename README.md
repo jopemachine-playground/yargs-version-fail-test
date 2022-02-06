d7639ed09c269b5dab5a44d15be4249396f7e508
Gyubong Lee <jopemachine@naver.com>
2022-02-06 13:25:13 +0900

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# yargs-version-fail-test
+
+```
+// print version correctly (1.0.4)
+$ node index.mjs --version
+
+// print 'unknown'
+$ npm i -g yargs-version-fail-test && yargs-version-fail-test --version
+```
---
+++ index.mjs
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+// https://github.com/yargs/yargs#esm
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+
+yargs(hideBin(process.argv))
+  .command('curl <url>', 'fetch the contents of the URL', () => {}, (argv) => {
+    console.info(argv)
+  })
+  .demandCommand(1)
+  .parse();
